8d2642e033c57421cb5d5ef743d06a862bda349a
Pedro Henrique <pedro00dk@gmail.com>
2018-01-17 15:11:11 -0300

    Add nodejs and react stuff

--- willow/urls.py
+++ willow/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import generic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', generic.TemplateView.as_view(template_name='index.html'))
 ]
