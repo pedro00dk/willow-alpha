815d833ff3dd39333107f3949114fa681f2e2fc0
Pedro Henrique <pedro00dk@gmail.com>
2018-01-18 12:14:08 -0300

    Create http request reducer

--- reactjs/containers/AppContainer.jsx
+++ reactjs/containers/AppContainer.jsx
@@ -4,7 +4,9 @@ import Radium from "radium"
 import { connect } from "react-redux"
 
 import * as counterActions from "../actions/counterActions"
+import * as githubActions from "../actions/githubActions"
 import Headline from "../components/Headline"
+import GithubRepos from "../components/GithubRepos"
 
 const styles = {
   button: {
@@ -18,16 +20,40 @@ const styles = {
 
 @connect(state => ({
   counters: state.counters,
+  github: state.github
 }))
 @Radium
 export default class AppContainer extends React.Component {
+
+  componentDidMount() {
+    let { dispatch, github } = this.props
+    if (!github.isLoadingRepos && github.repos === undefined) {
+      dispatch(githubActions.fetchRepos())
+    }
+  }
+
   handleClick() {
     let { dispatch } = this.props;
     dispatch(counterActions.increaseCounter())
   }
 
+  renderLoading() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            Loading...
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   render() {
-    let { counters } = this.props
+    let { counters, github } = this.props
+    if (github.isLoadingRepos || github.repos === undefined) {
+      return this.renderLoading()
+    }
     return (
       <div className="container">
         <div className="row">
@@ -36,6 +62,7 @@ export default class AppContainer extends React.Component {
             <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
             <p style={[styles.counter]}> {counters.clicks}</p>
             <p>{process.env.BASE_API_URL}</p>
+            {github.repos !== undefined && <GithubRepos repos={github.repos} />}
           </div>
         </div>
       </div>
--- reactjs/reducers/index.js
+++ reactjs/reducers/index.js
@@ -1,1 +1,2 @@
 export { default as counters } from './counters'
+export { default as github } from "./github"
