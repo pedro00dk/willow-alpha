c605f889be0cedaa3b479e219f31c5d0844ce01b
Pedro Henrique <pedro00dk@gmail.com>
2018-01-21 13:48:16 -0300

    Update project to support image import

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -23,7 +23,15 @@ module.exports = {
   ], // add all common plugins here
 
   module: {
-    loaders: [] // add all common loaders here
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug'
+        ]
+      }
+    ] // add all common loaders here
   },
 
   resolve: {
