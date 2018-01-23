9a0f35fb639d87bb2cf440ae9ea2db4efd480fa2
Pedro Henrique <pedro00dk@gmail.com>
2018-01-23 14:28:42 -0300

    Create Exercise model
    Some data too was added to teste front-bacl connection.

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import os
 
-# create your models here
+from django.db import models
+from willow.settings import DATABASES_DIR
+
+
+DATABASE_FILES_DIR = os.path.join(DATABASES_DIR, 'files')
+
+def exercises_file_readme_uploader(instance, filename):
+    return os.path.join(DATABASE_FILES_DIR, f'{type(instance).__name__}/{instance.id}/README.txt')
+
+def exercises_file_input_ex_uploader(instance, filename):
+    return os.path.join(DATABASE_FILES_DIR, f'{type(instance).__name__}/{instance.id}/input_ex.txt')
+
+def exercises_file_input_uploader(instance, filename):
+    return os.path.join(DATABASE_FILES_DIR, f'{type(instance).__name__}/{instance.id}/input.txt')
+
+class Exercise(models.Model):
+    name = models.CharField(unique=True, max_length=30)
+    readme = models.FileField(upload_to=exercises_file_readme_uploader)
+    input_ex = models.FileField(upload_to=exercises_file_input_ex_uploader)
+    imput = models.FileField(upload_to=exercises_file_input_uploader)
