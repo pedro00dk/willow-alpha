a96eda48b1de657d75f8789fc5aedf5c663ae6ee
Pedro Henrique <pedro00dk@gmail.com>
2018-01-25 16:05:25 -0300

    Create simple view to test front-back connection

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.views import generic
 
+from . import views
+
 
 urlpatterns = [
-    path('', generic.TemplateView.as_view(template_name='index.html'))
+    path('', generic.TemplateView.as_view(template_name='index.html')),
+    path('run', views.Run.as_view(), name='run')
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+
+class Run(View):
+
+    def get(self, request, *args, **kwargs):
+        print(request.GET)
+        return JsonResponse({'result': 'ok'})
+
+    def post(self, request, *args, **kwargs):
+        print(request.body)
+        return JsonResponse({'result': 'ok'})
