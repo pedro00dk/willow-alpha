4d81702abb0cbc416a66bb625cabfb0038b59534
Pedro Henrique <pedro00dk@gmail.com>
2018-01-26 19:00:03 -0300

    Implement REST serializer-viewset class generator
    With this generator, is no more needed write a serializer and a viewset for each model, they are now automaticaly generated.

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import include, path, re_path
 from django.views import generic
+from rest_framework import routers
 
-from . import views
+from backend.views import generated_viewsets
 
+
+router = routers.DefaultRouter()
+for name, viewset in generated_viewsets.items():
+    router.register(name, viewset)
 
 urlpatterns = [
     path('', generic.TemplateView.as_view(template_name='index.html')),
-    path('run', views.Run.as_view(), name='run')
+
+    re_path(r'^', include(router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,14 +1,34 @@
+from django.db.models import Model
+from rest_framework import serializers
+from rest_framework import viewsets
 
-from django.views import View
-from django.http import JsonResponse
+from backend import models
 
 
-class Run(View):
+def model_serializer_factory(model, base_serializer):
+    meta = type('Meta', (object,), {})
+    meta.model = model
+    meta.fields = '__all__'
+    serializer = type(f'{model.__name__}Serializer', (base_serializer,), {})
+    serializer.Meta = meta
+    return serializer
 
-    def get(self, request, *args, **kwargs):
-        print(request.GET)
-        return JsonResponse({'result': 'ok'})
 
-    def post(self, request, *args, **kwargs):
-        print(request.body)
-        return JsonResponse({'result': 'ok'})
+def model_viewset_factory(model, serializer, base_viewset):
+    viewset = type(f'{model.__name__}ViewSet', (base_viewset,), {})
+    viewset.queryset = model.objects.all()
+    viewset.serializer_class = serializer
+    return viewset
+
+
+base_serializer = serializers.HyperlinkedModelSerializer
+base_viewset = viewsets.ModelViewSet
+
+
+generated_serializers = {model.__name__: model_serializer_factory(model, base_serializer)
+                         for model in models.__dict__.values()
+                         if isinstance(model, type) and issubclass(model, Model)}
+
+generated_viewsets = {model.__name__: model_viewset_factory(model, generated_serializers[model.__name__], base_viewset)
+                      for model in models.__dict__.values()
+                      if isinstance(model, type) and issubclass(model, Model)}
