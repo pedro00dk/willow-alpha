942fa3239761929f9ee47afd830dc5daee38b288
Pedro Henrique <pedro00dk@gmail.com>
2018-02-01 16:27:53 -0300

    Add auth and redirect path to urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -6,14 +6,16 @@ from backend import views
 
 
 viewsets_routes = routers.DefaultRouter()
-viewsets_routes.register(r'^users', views.UserViewSet)
-viewsets_routes.register(r'^groups', views.GroupViewSet)
-viewsets_routes.register(r'^exercises', views.ExerciseViewset)
+viewsets_routes.register('users', views.UserViewSet)
+viewsets_routes.register('groups', views.GroupViewSet)
+viewsets_routes.register('exercises', views.ExerciseViewset)
 
 urlpatterns = [
     path('', generic.TemplateView.as_view(template_name='index.html')),
-    re_path(r'^', include(viewsets_routes.urls)),
-    re_path(r'^tracer', views.TracerAPIView.as_view()),
+    path('', include(viewsets_routes.urls)),
+    path('tracer', views.TracerAPIView.as_view()),
 
-    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^auth/', include('social_django.urls', namespace='social')),
+    path('auth/complete/google-oauth2/', generic.RedirectView.as_view(url='/'))
 ]
