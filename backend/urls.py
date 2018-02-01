0e74cf9347335f5db634a2ee249712592e0e4d54
Pedro Henrique <pedro00dk@gmail.com>
2018-02-01 18:04:30 -0300

    Add current_user view api

--- backend/urls.py
+++ backend/urls.py
@@ -12,10 +12,12 @@ viewsets_routes.register('exercises', views.ExerciseViewset)
 
 urlpatterns = [
     path('', generic.TemplateView.as_view(template_name='index.html')),
-    path('', include(viewsets_routes.urls)),
-    path('tracer', views.TracerAPIView.as_view()),
 
-    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    re_path(r'^auth/', include('social_django.urls', namespace='social')),
-    path('auth/complete/google-oauth2/', generic.RedirectView.as_view(url='/'))
+    path('', include(viewsets_routes.urls)),
+
+    re_path(r'^current_user', views.CurrentUserAPIView.as_view()),
+    re_path(r'^tracer', views.TracerAPIView.as_view()),
+
+    #re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^auth/', include('social_django.urls', namespace='social'))
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Group, User
 from django.db.models import Model
-from rest_framework import response, views, viewsets
+from rest_framework import permissions, response, views, viewsets
 from rest_framework.parsers import JSONParser
 
 from backend import models, serializers
@@ -8,16 +8,32 @@ from backend.tracer import tracer
 
 
 class UserViewSet(viewsets.ModelViewSet):
+
+    permission_classes = (permissions.IsAdminUser,)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = serializers.UserSerializer
 
 
+class CurrentUserAPIView(views.APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        instance = User.objects.get(id=request.user.id)
+        serializer = serializers.UserSerializer(instance)
+        return response.Response(serializer.data)
+        
+
 class GroupViewSet(viewsets.ModelViewSet):
+
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Group.objects.all()
     serializer_class = serializers.GroupSerializer
 
 
 class ExerciseViewset(viewsets.ModelViewSet):
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = models.Exercise.objects.all()
     serializer_class = serializers.ExerciseSerializer
 
