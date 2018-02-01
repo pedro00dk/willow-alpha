b66e967871cb921937c0aa4ff5edd8cbc5f4d847
Pedro Henrique <pedro00dk@gmail.com>
2018-02-01 11:31:59 -0300

    Fix backend reference errors
    Caused by module renaming (interpreter -> tracer).

--- backend/views.py
+++ backend/views.py
@@ -4,20 +4,16 @@ from rest_framework import response, views, viewsets
 from backend import models
 from backend import serializers
 
-from backend.interprerter.interpreter import Interpreter
-
 
 class ExerciseViewset(viewsets.ModelViewSet):
     queryset = models.Exercise.objects.all()
     serializer_class = serializers.ExerciseSerializer
 
 
-class InterpreterAPIView(views.APIView):
+class TracerAPIView(views.APIView):
 
     def get(self, request, format=None):
         return response.Response({'results': 'some data'})
 
     def post(self, request, format=None):
-        interpreter = Interpreter(request.data['script'])
-        result = interpreter.trace()
-        return response.Response({'result': result})
+        return response.Response({'result': 'traced data'})
