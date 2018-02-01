54d1a2bc733d215b30df8c725373e44491ae73e7
Pedro Henrique <pedro00dk@gmail.com>
2018-02-01 13:10:45 -0300

    Merge util and tracer scripts due import ptoblems

--- backend/tracer/tracer.py
+++ backend/tracer/tracer.py
@@ -3,8 +3,7 @@ import queue
 import sys
 import threading
 import traceback
-
-import util
+from functools import reduce
 
 
 class Tracer:
@@ -100,6 +99,70 @@ def _start_tracer_subprocess(script, info_main_to_sub_queue, info_sub_to_main_qu
     )
 
 
+class SubprocessInput:
+
+    def __init__(self, io_main_to_sub_queue, io_sub_to_main_queue):
+        self.io_main_to_sub_queue = io_main_to_sub_queue
+        self.io_sub_to_main_queue = io_sub_to_main_queue
+
+    def __call__(self, prompt=''):
+        prompt = str(prompt)
+        self.io_sub_to_main_queue.put(('input', prompt))
+        return self.io_main_to_sub_queue.get()
+
+
+class SubprocessPrint:
+
+    def __init__(self, io_main_to_sub_queue, io_sub_to_main_queue):
+        self.io_main_to_sub_queue = io_main_to_sub_queue
+        self.io_sub_to_main_queue = io_sub_to_main_queue
+
+    def __call__(self, *values, sep=None, end=None):
+        if sep is not None and type(sep) is not str:
+            raise TypeError(f'sep must be None or a string, not {type(end)}')
+        sep = sep if sep is not None else ' '
+        if end is not None and type(end) is not str:
+            raise TypeError(f'end must be None or a string, not {type(end)}')
+        end = end if end is not None else '\n'
+        values = values if len(values) > 0 else ('',)
+        text = reduce((lambda e0, e1: e0 + sep + e1), map((lambda e: str(e)), values)) + end
+        self.io_sub_to_main_queue.put(('print', text))
+
+
+def subprocess_disable_modules(modules):
+    sys = __import__('sys')
+    for module in sys.modules:
+        if module in modules:
+            sys.modules[module] = None
+
+
+class DisabledFunction:
+
+    def __init__(self, function_name):
+        self.function_name = function_name
+
+    def __call__(self, *args, **kwargs):
+        raise Exception(f'function {self.function_name} is disabled!!')
+
+
+def subprocess_disable_functions(functions, globals_data=globals()):
+    for f in functions:
+        globals_data['__builtins__'][f] = DisabledFunction(f)
+
+
+COMMON_DISABLE_MODULES = {
+    'sys', '_frozen_importlib', '_imp', '_warnings', '_thread', '_weakref',
+    '_frozen_importlib_external', '_io', 'marshal', 'nt', 'winreg', 'zipimport', 'encodings',
+    'codecs', '_codecs', 'encodings.aliases', 'encodings.utf_8', '_signal', '__main__',
+    'encodings.latin_1', 'io', 'abc', '_weakrefset', 'site', 'os', 'errno', 'stat', '_stat',
+    'ntpath', 'genericpath', 'os.path', '_collections_abc', '_sitebuiltins', 'sysconfig',
+    'atexit, multiprocessing, threading'
+}
+
+
+COMMON_DISABLE_FUNCTIONS = {'compile', 'exec'}
+
+
 class _SubprocessTracer:
 
     def __init__(self, script, info_main_to_sub_queue, info_sub_to_main_queue,
@@ -128,14 +191,14 @@ class _SubprocessTracer:
             '__cached__': None,
             '__builtins__': globals()['__builtins__'].copy()
         }
-        script_globals['__builtins__']['input'] = util.SubprocessInput(
+        script_globals['__builtins__']['input'] = SubprocessInput(
             self.io_main_to_sub_queue, self.io_sub_to_main_queue
         )
-        script_globals['__builtins__']['print'] = util.SubprocessPrint(
+        script_globals['__builtins__']['print'] = SubprocessPrint(
             self.io_main_to_sub_queue, self.io_sub_to_main_queue
         )
-        util.subprocess_disable_modules(util.COMMON_DISABLE_MODULES)
-        util.subprocess_disable_functions(util.COMMON_DISABLE_FUNCTIONS, script_globals)
+        subprocess_disable_modules(COMMON_DISABLE_MODULES)
+        subprocess_disable_functions(COMMON_DISABLE_FUNCTIONS, script_globals)
         #
         sys.settrace(self.trace_dispatch)
         try:
