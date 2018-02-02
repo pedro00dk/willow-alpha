63da926cda937e1599fa70d639c19b543569c512
Pedro Henrique <pedro00dk@gmail.com>
2018-02-02 17:32:00 -0300

    Finnaly a working reliable tracer script
    Lots of fixes, stop and end are now working, the input and print events are stacked with frame events in the next_event function.

--- backend/tracer/tracer.py
+++ backend/tracer/tracer.py
@@ -1,121 +1,44 @@
-import multiprocessing
 import queue
+import multiprocessing
 import sys
-import threading
 import traceback
 from functools import reduce
 
 
-class Tracer:
+_ACTION_NEXT = 'next'
+_ACTION_QUIT = 'quit'
 
-    def __init__(self, script, input_callback, print_callback):
-        self._script = script
-        self._script_lines = script.splitlines()
-        self._input_callback = input_callback
-        self._print_callback = print_callback
-        self._ended = False
-        self._info_main_to_sub_queue = multiprocessing.Queue()
-        self._info_sub_to_main_queue = multiprocessing.Queue()
-        self._io_main_to_sub_queue = multiprocessing.Queue()
-        self._io_sub_to_main_queue = multiprocessing.Queue()
-        self._tracer_subprocess = multiprocessing.Process(
-            target=_start_tracer_subprocess,
-            args=(
-                self._script,
-                self._info_main_to_sub_queue, self._info_sub_to_main_queue,
-                self._io_main_to_sub_queue, self._io_sub_to_main_queue
-            )
-        )
-        self._tracer_subprocess.start()
-        self._tracer_io_thread = threading.Thread(target=self._start_io, args=())
-        self._tracer_io_thread.start()
+_EVENT_FRAME = 'frame'
+_EVENT_INPUT = 'input'
+_EVENT_PRINT = 'print'
+_EVENT_QUIT = 'quit'
 
-    def next_info(self):
-        if self._ended or not self._tracer_subprocess.is_alive():
-            raise Exception('tracer ended')
-        self._info_main_to_sub_queue.put('next')
-        info = self._info_sub_to_main_queue.get()
-        if info['end']:
-            self._ended = True
-        return info
+_FRAME_CALL = 'call'
+_FRAME_LINE = 'line'
+_FRAME_EXCEPTION = 'exception'
+_FRAME_RETURN = 'return'
+_FRAME_EVENTS = {_FRAME_CALL, _FRAME_LINE, _FRAME_EXCEPTION, _FRAME_RETURN}
 
-    def _stop(self):
-        if self._ended or not self._tracer_subprocess.is_alive():
-            raise Exception('tracer ended')
-        self._info_main_to_sub_queue.put('quit')
-        return self._info_sub_to_main_queue.get() == 'quit'
-
-    def _start_io(self):
-        timeout = 0.01
-        while self._tracer_subprocess.is_alive():
-            try:
-                io_action, value = self._io_sub_to_main_queue.get(timeout=timeout)
-                if io_action == 'input':
-                    expected = self._input_callback(value)
-                    self._io_main_to_sub_queue.put(expected)
-                elif io_action == 'print':
-                    self._print_callback(value)
-            except queue.Empty:
-                pass
+_SCRIPT_NAME = '<string>'
 
 
-class CompressedTracer(Tracer):
+class _Input:
 
-    def __init__(self, script, input_callback, print_callback):
-        super().__init__(script, input_callback, print_callback)
-        self.saved_info = None
-
-    def next_info(self):
-        if self._ended or not self._tracer_subprocess.is_alive():
-            raise Exception('tracer ended')
-        while True:
-            self._info_main_to_sub_queue.put('next')
-            info = self._info_sub_to_main_queue.get()
-            if info['end']:
-                self._ended = True
-                return info
-            if info['filename'] != '<string>':
-                continue
-            if self.saved_info is None:
-                self.saved_info = info
-            else:
-                if info['line'] == self.saved_info['line']:
-                    self.saved_info = info
-                else:
-                    final_info = self.saved_info
-                    self.saved_info = info
-                    return final_info
-        return info
-    
-
-# everything below run in other process
-
-def _start_tracer_subprocess(script, info_main_to_sub_queue, info_sub_to_main_queue,
-                             io_main_to_sub_queue, io_sub_to_main_queue):
-    _SubprocessTracer(
-        script,
-        info_main_to_sub_queue, info_sub_to_main_queue,
-        io_main_to_sub_queue, io_sub_to_main_queue
-    )
-
-
-class SubprocessInput:
-
-    def __init__(self, io_main_to_sub_queue, io_sub_to_main_queue):
-        self.io_main_to_sub_queue = io_main_to_sub_queue
-        self.io_sub_to_main_queue = io_sub_to_main_queue
+    def __init__(self, main_to_sub_queue, sub_to_main_queue):
+        self.main_to_sub_queue = main_to_sub_queue
+        self.sub_to_main_queue = sub_to_main_queue
 
     def __call__(self, prompt=''):
         prompt = str(prompt)
-        self.io_sub_to_main_queue.put(('input', prompt))
-        return self.io_main_to_sub_queue.get()
+        self.sub_to_main_queue.put((_EVENT_INPUT, prompt))
+        return self.main_to_sub_queue.get()
 
 
-class SubprocessPrint:
+class _Print:
 
-    def __init__(self, io_main_to_sub_queue, io_sub_to_main_queue):
-        self.io_main_to_sub_queue = io_main_to_sub_queue
-        self.io_sub_to_main_queue = io_sub_to_main_queue
+    def __init__(self, main_to_sub_queue, sub_to_main_queue):
+        self.main_to_sub_queue = main_to_sub_queue
+        self.sub_to_main_queue = sub_to_main_queue
 
     def __call__(self, *values, sep=None, end=None):
         if sep is not None and type(sep) is not str:
@@ -126,17 +49,10 @@ class SubprocessPrint:
         end = end if end is not None else '\n'
         values = values if len(values) > 0 else ('',)
         text = reduce((lambda e0, e1: e0 + sep + e1), map((lambda e: str(e)), values)) + end
-        self.io_sub_to_main_queue.put(('print', text))
+        self.sub_to_main_queue.put((_EVENT_PRINT, text))
 
 
-def subprocess_disable_modules(modules):
-    sys = __import__('sys')
-    for module in sys.modules:
-        if module in modules:
-            sys.modules[module] = None
-
-
-class DisabledFunction:
+class _DisabledFunction:
 
     def __init__(self, function_name):
         self.function_name = function_name
@@ -145,12 +61,20 @@ class DisabledFunction:
         raise Exception(f'function {self.function_name} is disabled!!')
 
 
-def subprocess_disable_functions(functions, globals_data=globals()):
+def _disable_functions(functions, globals_data=globals()):
     for f in functions:
-        globals_data['__builtins__'][f] = DisabledFunction(f)
+        globals_data['__builtins__'][f] = _DisabledFunction(f)
 
 
-COMMON_DISABLE_MODULES = {
+def _disable_modules(modules):
+    sys_module = __import__('sys')
+    for module in sys.modules:
+        if module in modules:
+            sys_module.modules[module] = None
+
+
+_COMMON_DISABLE_FUNCTIONS = {'compile', 'exec'}
+_COMMON_DISABLE_MODULES = {
     'sys', '_frozen_importlib', '_imp', '_warnings', '_thread', '_weakref',
     '_frozen_importlib_external', '_io', 'marshal', 'nt', 'winreg', 'zipimport', 'encodings',
     'codecs', '_codecs', 'encodings.aliases', 'encodings.utf_8', '_signal', '__main__',
@@ -160,49 +84,136 @@ COMMON_DISABLE_MODULES = {
 }
 
 
-COMMON_DISABLE_FUNCTIONS = {'compile', 'exec'}
+class TracerController:
+
+    def __init__(self, script):
+        self._script = script
+        self._script_lines = script.splitlines()
+        self._main_to_sub_queue = multiprocessing.Queue()
+        self._sub_to_main_queue = multiprocessing.Queue()
+        self._io_main_to_sub_queue = multiprocessing.Queue()
+        self._io_sub_to_main_queue = multiprocessing.Queue()
+        self._tracer_subprocess = multiprocessing.Process(
+            target=_start_tracer_process,
+            args=(
+                self._script,
+                self._main_to_sub_queue, self._sub_to_main_queue,
+                self._io_main_to_sub_queue, self._io_sub_to_main_queue
+            )
+        )
+        self._tracer_subprocess.start()
+        self.end = False
+        self._last_event = _EVENT_FRAME
+
+    def next_event(self):
+        if self.end:
+            raise Exception('tracer ended')
+        if self._last_event == _EVENT_FRAME:
+            self._main_to_sub_queue.put(_ACTION_NEXT)
+            while True:
+                try:
+                    event, value = self._io_sub_to_main_queue.get_nowait()
+                    break
+                except queue.Empty:
+                    pass
+                try:
+                    event, value = self._sub_to_main_queue.get_nowait()
+                    break
+                except queue.Empty:
+                    pass
+            self._last_event = event
+            if event == _EVENT_FRAME:
+                self.end = value['end']
+                if self.end:
+                    self._tracer_subprocess.join()
+                return event, value
+            elif event == _EVENT_INPUT:
+                return event, value, lambda i: self._io_main_to_sub_queue.put(i)
+            elif event == _EVENT_PRINT:
+                return event, value
+        elif self._last_event == _EVENT_INPUT or self._last_event == _EVENT_PRINT:
+            event, value = self._sub_to_main_queue.get()
+            self._last_event = event
+            return event, value
+
+    def stop(self):
+        if self.end:
+            raise Exception('tracer ended')
+        quit = None
+        if self._last_event == _EVENT_FRAME:
+            self._main_to_sub_queue.put(_ACTION_QUIT)
+            quit = self._sub_to_main_queue.get()[0] == _EVENT_QUIT
+        elif self._last_event == _EVENT_INPUT or self._last_event == _EVENT_PRINT:
+            event, value = self._sub_to_main_queue.get()
+            self._main_to_sub_queue.put(_ACTION_QUIT)
+            quit = self._sub_to_main_queue.get()[0] == _EVENT_QUIT
+        self.end = True
+        self._main_to_sub_queue.close()
+        self._sub_to_main_queue.close()
+        self._io_main_to_sub_queue.close()
+        self._io_sub_to_main_queue.close()
+        # the traceback does not stop if the program does not finish, the process should be killed
+        self._tracer_subprocess.terminate()
+        self._tracer_subprocess.join()
 
 
-class _SubprocessTracer:
+class FilteredTracerController(TracerController):
 
-    def __init__(self, script, info_main_to_sub_queue, info_sub_to_main_queue,
-                 io_main_to_sub_queue, io_sub_to_main_queue):
+    def __init__(self, script):
+        super().__init__(script)
+        self.saved_event = None
+
+    def next_event(self):
+        while True:
+            event_value = super().next_event()
+            event = event_value[0]
+            if event == _EVENT_INPUT or event == _EVENT_PRINT:
+                return event_value
+            if event_value[1]['filename'] != _SCRIPT_NAME:
+                continue
+            if self.saved_event is None:
+                self.saved_event = event_value
+            else:
+                if event_value[1]['line'] == self.saved_event[1]['line']:
+                    self.saved_event = event_value
+                else:
+                    result_event = self.saved_event
+                    self.saved_event = event_value
+                    return result_event
+
+
+def _start_tracer_process(script, main_to_sub_queue, sub_to_main_queue, io_main_to_sub_queue, io_sub_to_main_queue):
+    _TracerProcess(script, main_to_sub_queue, sub_to_main_queue, io_main_to_sub_queue, io_sub_to_main_queue)
+
+
+class _TracerProcess:
+
+    def __init__(self, script, main_to_sub_queue, sub_to_main_queue, io_main_to_sub_queue, io_sub_to_main_queue):
         self.script = script
         self.script_lines = script.splitlines()
-        self.info_main_to_sub_queue = info_main_to_sub_queue
-        self.info_sub_to_main_queue = info_sub_to_main_queue
+        self.main_to_sub_queue = main_to_sub_queue
+        self.sub_to_main_queue = sub_to_main_queue
         self.io_main_to_sub_queue = io_main_to_sub_queue
         self.io_sub_to_main_queue = io_sub_to_main_queue
         self.bot_frame = None
+        self.quit = False
         self.start()
 
     def start(self):
-        #
         compile_backup = compile
         exec_backup = exec
-        #
         script_globals = {
-            '__name__': '__main__',
-            '__file__': '<script>',
-            '__doc__': None,
-            '__package__': None,
-            '__loader__': None,
-            '__spec__': None,
-            '__cached__': None,
+            '__name__': '__main__', '__file__': '<string>', '__doc__': None, '__package__': None,
+            '__loader__': None, '__spec__': None, '__cached__': None,
             '__builtins__': globals()['__builtins__'].copy()
         }
-        script_globals['__builtins__']['input'] = SubprocessInput(
-            self.io_main_to_sub_queue, self.io_sub_to_main_queue
-        )
-        script_globals['__builtins__']['print'] = SubprocessPrint(
-            self.io_main_to_sub_queue, self.io_sub_to_main_queue
-        )
-        subprocess_disable_modules(COMMON_DISABLE_MODULES)
-        subprocess_disable_functions(COMMON_DISABLE_FUNCTIONS, script_globals)
-        #
+        script_globals['__builtins__']['input'] = _Input(self.io_main_to_sub_queue, self.io_sub_to_main_queue)
+        script_globals['__builtins__']['print'] = _Print(self.io_main_to_sub_queue, self.io_sub_to_main_queue)
+        _disable_modules(_COMMON_DISABLE_MODULES)
+        _disable_functions(_COMMON_DISABLE_FUNCTIONS, script_globals)
         sys.settrace(self.trace_dispatch)
         try:
-            compiled_script = compile_backup(self.script, '<string>', 'exec')
+            compiled_script = compile_backup(self.script, _SCRIPT_NAME, 'exec')
             exec_backup(compiled_script, script_globals)
         except Exception:
             pass
@@ -210,61 +221,62 @@ class _SubprocessTracer:
             sys.settrace(None)
 
     def trace_dispatch(self, frame, event, args):
-        action = self.info_main_to_sub_queue.get()
-        if action == 'next':
+        if self.quit:
+            return None
+        if event not in _FRAME_EVENTS:
+            return self.trace_dispatch
+        action = self.main_to_sub_queue.get()
+        if action == _ACTION_NEXT:
             if self.bot_frame is None:
                 self.bot_frame = frame
-            self.info_sub_to_main_queue.put(self.get_frame_data(frame, event, args))
+            data = self.get_frame_data(frame, event, args)
+            self.sub_to_main_queue.put((_EVENT_FRAME, self.get_frame_data(frame, event, args)))
             return self.trace_dispatch
-        elif action == 'quit':
-            self.info_sub_to_main_queue.put('quit')
+        elif action == _ACTION_QUIT:
+            self.quit = True
+            self.sub_to_main_queue.put((_EVENT_QUIT, None))
             return None
 
     def get_frame_data(self, frame, event, args):
         filename = frame.f_code.co_filename
         line = frame.f_lineno - 1
-        text = self.script_lines[line] if filename == '<string>' else '#'
+        text = self.script_lines[line] if filename == _SCRIPT_NAME else '#'
         stack, depth = self.get_stack(frame)
-        end = event == 'return' and depth <= 1
-
-        if event == 'exception':
-            # tracebacks (args[2]) are not 'picklable'
-            args = (
-                args[0],
-                args[1],
-                traceback.format_exception(args[0], args[1], args[2]))
-
+        end = event == _FRAME_RETURN and depth <= 1
+        if event == _FRAME_EXCEPTION:
+            args = (args[0], args[1], traceback.format_exception(args[0], args[1], args[2]))
         return {
             'event': event,
-            'args': args if filename == '<string>' else [],
+            'args': args if filename == _SCRIPT_NAME else [],
             'filename': filename,
             'line': line,
             'stack': stack,
             'depth': depth,
             'text': text,
-            'end': end}
+            'end': end
+        }
 
     def get_stack(self, frame):
         stack = []
         while frame is not None:
             filename = frame.f_code.co_filename
             line = frame.f_lineno - 1
-            text = self.script_lines[line] if filename == '<string>' else '#'
+            text = self.script_lines[line] if filename == _SCRIPT_NAME else '#'
             stack.append({
                 'filename': filename,
                 'line': line,
-                'text': text}),
+                'text': text
+            }),
             if frame is self.bot_frame:
                 break
             frame = frame.f_back
         stack.reverse()
-        i = max(0, len(stack))
-        return stack, i
+        depth = max(0, len(stack))
+        return stack, depth
 
 
 ################################
-def main():
-    script = '''
+script = '''
 
 from functools import reduce
 try:
@@ -288,35 +300,56 @@ class XYZ:
 x = 10
 y = 10
 z = 10
-
-x = int(input('x value: '))
-print(f'x: {x}')
-y = int(input('y value: '))
-print(f'y: {y}')
-z = int(input('z value: '))
-print(f'z: {z}')
-
 v = XYZ(x, y, z)
 sm = v.sqr_mag()
 print(f'sqr_mag of {v} is {sm}')
+
+
+#for i in range(100000):
+#    x = f'{i} sqr_mag of {v} is {sm}'
+#    if i % 100 == 0:
+#        j = input('input')
+#        print(j + ' ' + x)
+
+for i in range(10000000000):
+    if i % 10 == 0:
+        pass
+        print(([i] * i))
+
+
+
 
 raise 'error'
 
 '''
 
-    def input_callback(prompt):
-        return input(prompt)
 
-    def print_callback(text):
-        print(text, end='')
-
-    t = CompressedTracer(script, input_callback, print_callback)
+def main():
+    t = TracerController(script)
+    print_count = 0
     while True:
-        try:
-            info = t.next_info()
-            print(info['text'])
-        except Exception:
+        if print_count == 10:
+            t.stop()
             break
+        try:
+            event_value = t.next_event()
+            event = event_value[0]
+            if event == 'frame':
+                if event_value[1]['event'] == 'exception':
+                    print('exception')
+                if event_value[1]['filename'] != _SCRIPT_NAME:
+                    continue
+                pass
+            if event == 'print':
+                print_count += 1
+                print(event_value[1], end='')
+            if event == 'input':
+                event_value[2]('100')
+        except Exception as e:
+            print(e)
+            break
+    print('end')
+    print(t._tracer_subprocess.is_alive())
 
 
 if __name__ == '__main__':
