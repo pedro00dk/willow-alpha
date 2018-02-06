e4a836579a2aca2789294ae6201a664b0bd3ea9d
Pedro Henrique <pedro00dk@gmail.com>
2018-02-06 17:39:22 -0300

    Refactor TracerAPIWiew in views.py

--- backend/views.py
+++ backend/views.py
@@ -70,70 +70,89 @@ class TracerAPIView(views.APIView):
     user_tracers = {}
 
     def post(self, request, format=None):
-        if 'option' not in request.data:
+        option, error_response = self.require_option(request, 'option')
+        if error_response is not None:
+            return error_response
+        if option == 'start' or option == 'restart':
+            return self.start_tracer(request)
+        if option == 'stop':
+            return self.stop_tracer(request)
+        if option == 'input':
+            return self.send_input(request)
+        if option == 'stepinto' or option == 'stepover' or option == 'stepout':
+            return self.tracer_step(request, option)
+        return response.Response(
+            {'detail': 'option not supported'}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def require_option(self, request, option_name):
+        option = request.data.get(option_name)
+        error_response = None if option is not None else response.Response(
+            {'detail': f'{option_name} not provided'}, status=status.HTTP_400_BAD_REQUEST
+        )
+        return option, error_response
+
+    def require_tracer(self, request):
+        user_tracer = TracerAPIView.user_tracers.get(request.user.id)
+        error_response = None if user_tracer is not None else response.Response(
+            {'detail': 'no tracer running'}, status=status.HTTP_400_BAD_REQUEST
+        )
+        return user_tracer, error_response
+
+    def start_tracer(self, request):
+        script, error_response = self.require_option(request, 'script')
+        if error_response is not None:
+            return error_response
+        user_tracer, error_response = self.require_tracer(request)
+        if user_tracer is not None:
+            try:
+                user_tracer.stop()
+            except Exception as e:
+                pass
+        TracerAPIView.user_tracers[request.user.id] = tracer.StepTracerController(script)
+        return response.Response(
+            {'detail': f'tracer {"restarted" if user_tracer is not None else "started"}'}
+        )
+
+    def stop_tracer(self, request):
+        user_tracer, error_response = self.require_tracer(request)
+        if error_response is not None:
+            return error_response
+        try:
+            user_tracer.stop()
+        except Exception as e:
+            pass
+        TracerAPIView.user_tracers.pop(request.user.id)
+        return response.Response({'detail': 'tracer stopped'})
+
+    def send_input(self, request):
+        input_data, error_response = self.require_option(request, 'input')
+        if error_response is not None:
+            return error_response
+        user_tracer, error_response = self.require_tracer(request)
+        if error_response is not None:
+            return error_response
+        if type(input_data) == list:
+            for data in input_data:
+                user_tracer.send_input(str(data))
+        else:
+            user_tracer.send_input(str(input_data))
+        return response.Response({'detail': 'input received'})
+
+    def tracer_step(self, request, step_type):
+        user_tracer, error_response = self.require_tracer(request)
+        if error_response is not None:
+            return error_response
+        try:
+            if step_type == 'stepinto':
+                events = user_tracer.step_into()
+            if step_type == 'stepover':
+                events = user_tracer.step_over()
+            if step_type == 'stepout':
+                events = user_tracer.step_out()
+            return response.Response({'detail': step_type, 'events': events})
+        except Exception as e:
             return response.Response(
-                {'detail': 'option not provided or supported'},
+                {'detail': str(e), 'events': []},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        option = request.data['option']
-        user_tracer = TracerAPIView.user_tracers[request.user.id] \
-            if request.user.id in TracerAPIView.user_tracers else None
-        if option == 'start' or option == 'restart':
-            if 'script' not in request.data:
-                return response.Response(
-                    {'detail': 'script not provided'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            script = request.data['script']
-            is_restarting = user_tracer is not None
-            if is_restarting:
-                try:
-                    user_tracer.stop()
-                except Exception as e:
-                    pass
-            TracerAPIView.user_tracers[request.user.id] = tracer.StepTracerController(script)
-            return response.Response({'detail': f'tracer {"re" if is_restarting else ""}started'})
-        if option == 'stop':
-            if user_tracer is None:
-                return response.Response(
-                    {'detail': 'no tracer running'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            user_tracer.stop()
-            TracerAPIView.user_tracers.pop(request.user.id)
-            return response.Response({'detail': 'tracer stopped'})
-        if option == 'input':
-            if user_tracer is None:
-                return response.Response(
-                    {'detail': 'no tracer running'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            if 'input' not in request.data:
-                return response.Response(
-                    {'detail': 'input not provided'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            input_data = request.data['input']
-            if type(input_data) == list:
-                for data in input_data:
-                    user_tracer.send_input(str(data))
-            else:
-                user_tracer.send_input(str(input_data))
-            return response.Response({'detail': 'input received'})
-        if option == 'stepinto' or option == 'stepover' or option == 'stepout':
-            if user_tracer is None:
-                return response.Response(
-                    {'detail': 'no tracer running'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            if option == 'stepinto':
-                events = user_tracer.step_into()
-            if option == 'stepover':
-                events = user_tracer.step_over()
-            if option == 'stepout':
-                events = user_tracer.step_out()            
-            return response.Response({'detail': option, 'events': events})
-        return response.Response(
-            {'detail': 'option not provided or supported'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
