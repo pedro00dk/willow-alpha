719a7fcb2f83f1d13727450f960b18b2df2caee0
Pedro Henrique <pedro00dk@gmail.com>
2018-02-25 19:32:37 -0300

    Organize components in container

--- frontend/App/containers/AppContainer.jsx
+++ frontend/App/containers/AppContainer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import DebugBar from '../components/DebugBar'
 import Header from '../components/Header'
 import InputEditor from '../components/InputEditor'
+import Inspector from '../components/Inspector'
 import OutputEditor from '../components/OutputEditor'
 import ScriptEditor from '../components/ScriptEditor'
 
@@ -10,12 +11,27 @@ import ScriptEditor from '../components/ScriptEditor'
 export default class AppContainer extends React.Component {
 
     render() {
-        return <div>
-            <Header />
-            <DebugBar />
-            <ScriptEditor />
-            <InputEditor />
-            <OutputEditor />
+        return <div className='container-fluid p-0'>
+            <Header style={{ height: '8vh' }}/>
+            <div className='row m-0'>
+                <DebugBar style={{ height: '8vh' }}/>
+            </div>
+            <div className='row m-0'>
+                <div className='col-6 border p-0'>
+                    <ScriptEditor style={{ width: '100%', height: '60vh' }} />
+                </div>
+                <div className='col-6 border  p-0'>
+                    <Inspector />
+                </div>
+            </div>
+            <div className='row m-0'>
+                <div className='col-6 border p-0'>
+                    <InputEditor style={{ width: '100%', height: '23vh' }} />
+                </div>
+                <div className='col-6 border p-0'>
+                    <OutputEditor style={{ width: '100%', height: '23vh' }} />
+                </div>
+            </div>
         </div>
     }
 }
