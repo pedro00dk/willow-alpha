689c815a99b01e1d328a9cda6f8472efe4ff2030
Pedro Henrique <pedro00dk@gmail.com>
2018-02-26 17:30:09 -0300

    Remove d3 and faux DOM
    The inspector will be implemented using react only.

--- frontend/App/components/Inspector.jsx
+++ frontend/App/components/Inspector.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import ReactFauxDOM from 'react-faux-dom'
-import * as d3 from 'd3'
 
-
-@connect(state => ({debug: state.debug}))
+@connect(state => ({ debug: state.debug }))
 export default class Inspector extends React.Component {
 
     constructor(props) {
         super(props)
-
-        this.dom = ReactFauxDOM.createElement('svg')
-        this.svg = d3.select(this.dom)
-            .attr('width', '100%')
-            .attr('height', '100%')
     }
 
     render() {
-        let text = this.svg.append('text')
-            .text('Hello world!')
-            .attr('dy', '0.35em')
-            .attr('transform', 'translate(100, 100)');
-
-        return this.dom.toReact()
+        return <div></div>
     }
 }
