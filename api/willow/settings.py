7f694c7df557cc6d72dd67821a051fbd908e726a
Pedro Henrique <pedro00dk@gmail.com>
2018-06-10 14:27:19 -0300

    Add cross site support

--- api/willow/settings.py
+++ api/willow/settings.py
@@ -19,11 +19,13 @@ INSTALLED_APPS = [
 
     'backend',
 
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,3 +91,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ]
 }
+
+
+# CORS Configuration
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = ('localhost:1234', 'localhost:3000')
+CORS_ORIGIN_REGEX_WHITELIST = ('localhost:*',)
